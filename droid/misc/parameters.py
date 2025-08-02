125c0c2e3f050a849035e534c096751db6e77b9f
mpk-nuc <mpk-nuc@example.com>
2025-08-02 14:24:03 -0700

    fill paprameters for mpk

--- droid/misc/parameters.py
+++ droid/misc/parameters.py
@@ -2,12 +2,12 @@ import os
 from cv2 import aruco
 
 # Robot Params #
-nuc_ip = ""
-robot_ip = ""
-laptop_ip = ""
-sudo_password = ""
-robot_type = ""  # 'panda' or 'fr3'
-robot_serial_number = ""
+nuc_ip = "172.16.0.3"
+robot_ip = "172.16.0.2"
+laptop_ip = "172.16.0.1"
+sudo_password = "Meta@123"
+robot_type = "panda"  # 'panda' or 'fr3'
+robot_serial_number = "295341-1325109"
 
 # Camera ID's #
 hand_camera_id = ""
@@ -22,7 +22,7 @@ CHARUCOBOARD_MARKER_SIZE = 0.016
 ARUCO_DICT = aruco.Dictionary_get(aruco.DICT_5X5_100)
 
 # Ubuntu Pro Token (RT PATCH) #
-ubuntu_pro_token = ""
+# ubuntu_pro_token = ""
 
 # Code Version [DONT CHANGE] #
 droid_version = "1.3"
